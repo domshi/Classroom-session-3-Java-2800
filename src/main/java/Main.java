971419c75bf871cd8e3f394d15b28486b2c5f463
domshi <90562421+domshi@users.noreply.github.com>
2024-09-17 22:32:10 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    int a = 8;
+    int b = 8;
+    boolean c = (a>b);
+
+    
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(a>=b);
+    System.out.println(a!=b);
+    System.out.println(c);
+    
+    
+  }
+}
